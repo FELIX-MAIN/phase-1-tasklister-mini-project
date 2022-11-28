41013fef9b17ba420f32d022de6fc6af16fb3efe
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-29 01:31:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('#create-task-form').addEventListener('click', taskcreated)
+
+  function taskcreated() {
+    return alert ('Task created successfully')
+  }
+  function taskdelete(e) {
+    e.target.parentNode.remove()
+  }
 });
